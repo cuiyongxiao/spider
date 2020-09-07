f9d3a8ddc02fdc3171883966dd74d2639d6d2ae2
yongxiaocui <812229090@qq.com>
2020-09-07 17:13:58 +0800

    firsr

---
+++ __douyin/2 read_dir.py
@@ -0,0 +1,21 @@
+import os
+# for files in os.walk('/Users/yongxiaocui/Desktop/aweme/follower/list/'):
+#     # print(root)  # 当前目录路径
+#     # print(dirs)  # 当前路径下所有子目录
+#     print(1)
+#     print(files)  # 当前路径下所有非目录子文件、
+
+
+# os.walk('/Use
+
+#
+
+import os
+for files in os.walk('/Users/yongxiaocui/Desktop/list/'):
+    # print(root)  # 当前目录路径
+    # print(dirs)  # 当前路径下所有子目录
+    print(files)  # 当前路径下所有非目录子文件、
+    # for i in files:
+    #     print(i)
+    #     print(len(i))
+    # print(len(files))
